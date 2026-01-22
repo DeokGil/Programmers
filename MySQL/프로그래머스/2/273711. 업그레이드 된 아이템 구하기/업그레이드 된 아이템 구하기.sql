1c86dabb632ae802c8424093cb99433fc8eba229
DeokGil Han <67721179+DeokGil@users.noreply.github.com>
2026-01-22 16:12:07 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 작성해주세요 mysql
+select ITEM_ID, ITEM_NAME, RARITY
+from ITEM_INFO 
+where ITEM_ID in (
+    select 
+        TREE.ITEM_ID
+    from   
+              ITEM_INFO as INFO
+    left join ITEM_TREE as TREE on INFO.ITEM_ID = TREE.PARENT_ITEM_ID 
+    where
+    INFO.RARITY = 'RARE' and TREE.item_id is not null
+)
+order by ITEM_ID desc;
+
+
+
